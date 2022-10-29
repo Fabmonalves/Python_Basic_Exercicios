879e921245338a7a8841cf9c69576e6b72885e1b
Fabricio Monte <fab.alves46@gmail.com>
2022-10-29 15:40:52 -0300

    Python_Basic_Curso_Em_Video
    https://www.cursoemvideo.com/cursos/

--- script_python/desafios/desafio 03.py
+++
@@ -1,7 +0,0 @@
-numero01=input ('Digite um numero: ')
-numero02=input ('Digite outro numero: ')
-numero001=int(numero01)
-numero002=int(numero02)
-
-print(' a soma dos dois numeros é : ' + numero001+numero002)
-               
